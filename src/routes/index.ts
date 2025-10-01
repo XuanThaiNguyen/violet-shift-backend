658398019db3b213e550ca58f796d530e639b4d1
ngtsnn <tusocnau@gmail.com>
2025-10-01 17:16:06 +0700

    fix: update prefix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,13 @@ import { Express, Router } from "express";
 import authRoutes from "./authRoute";
 import healthzRoutes from "./healthzRoute";
 
-const prefix = process.env.PREFIX || "/";
+
 
 const router = Router();
 router.use('/auth', authRoutes);
 router.use('/healthz', healthzRoutes);
 
 export const route = (app: Express) => {
+    const prefix = process.env.PREFIX || "/";
     app.use(prefix, router);
 }
