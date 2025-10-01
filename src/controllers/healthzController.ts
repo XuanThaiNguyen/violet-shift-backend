cd7055c7500da2353af7a9e3c7e9fb98142f8524
ngtsnn <tusocnau@gmail.com>
2025-10-01 17:15:19 +0700

    fix: update prefix

--- src/controllers/healthzController.ts
+++ src/controllers/healthzController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { sendResponse } from "../utils/sendResponse";
 
 export const healthz = async (req: Request, res: Response) => {
+  console.log("🚀 ~ healthz:")
   return sendResponse({
     res,
     statusCode: 200,
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Express, Router } from "express";
 import authRoutes from "./authRoute";
+import healthzRoutes from "./healthzRoute";
 
 const prefix = process.env.PREFIX || "/";
 
 const router = Router();
 router.use('/auth', authRoutes);
+router.use('/healthz', healthzRoutes);
 
 export const route = (app: Express) => {
-    app.use('/prefix', router);
+    app.use(prefix, router);
 }
